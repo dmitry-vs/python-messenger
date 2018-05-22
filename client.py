634c4b9106633487d7b4b3acbcfb004e80b8ea2a
dmitry-vs <dmitryselin@inbox.ru>
2018-05-22 18:39:42 +0300

    Translating w-clients messages to r-clients works

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 import sys
-import socket as sck
+from socket import socket, AF_INET, SOCK_STREAM
 import argparse
 
 import helpers
@@ -10,20 +10,24 @@ def parse_commandline_args(cmd_args):
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', dest='server_ip', type=str, default='127.0.0.1', help='server ip, default 127.0.0.1')
     parser.add_argument('-p', dest='server_port', type=int, default=7777, help='server port, default 7777')
-    parser.add_argument('-w', dest='mode_write', action='store_true', default=False, help='client mode "write" (otherwise "read"), default False')
+    parser.add_argument('-w', dest='mode_write', action='store_true', default=False,
+                        help='client mode "write" (otherwise "read"), default False')
     return parser.parse_args(cmd_args)
 
 
 if __name__ == '__main__':
     args = parse_commandline_args(sys.argv[1:])
-    client_socket = sck.socket(sck.AF_INET, sck.SOCK_STREAM)
-    client_socket.connect((args.server_ip, args.server_port))
-    message = JimMessage()
-    message.set_field('action', 'presence')
-    message.set_time()
-    print('sending presence message to server')
-    client_socket.send(message.to_bytes())
-    response = client_socket.recv(helpers.TCP_MSG_BUFFER_SIZE)
-    server_message = jim_msg_from_bytes(response)
-    print(f'response from server: {server_message}')
-    client_socket.close()
+    with socket(AF_INET, SOCK_STREAM) as client_socket:
+        client_socket.connect((args.server_ip, args.server_port))
+        while True:
+            import time
+            if args.mode_write:
+                message = JimMessage()
+                message.set_field('action', 'presence')
+                message.set_time()
+                print(f'Sending message to server: {message}')
+                client_socket.send(message.to_bytes())
+                time.sleep(1)
+            else:
+                response = client_socket.recv(helpers.TCP_MSG_BUFFER_SIZE)
+                print(jim_msg_from_bytes(response))
--- server.py
+++ server.py
@@ -1,35 +1,70 @@
 import argparse
-import socket as sck
+from socket import socket, AF_INET, SOCK_STREAM
 import sys
+import select
 
 import helpers
-from jim import JimMessage, jim_msg_from_bytes
-
-CLIENTS_COUNT_LIMIT = 5
 
 
 def parse_commandline_args(cmd_args):
     parser = argparse.ArgumentParser()
-    parser.add_argument('-a', dest='listen_address', type=str, default='', help='ip-address to listen on')
-    parser.add_argument('-p', dest='listen_port', type=int, default=helpers.DEFAULT_SERVER_PORT, help='tcp port to listen on')
+    parser.add_argument('-a', dest='listen_address', type=str, default='', help='ip-address to listen on, default empty')
+    parser.add_argument('-p', dest='listen_port', type=int, default=helpers.DEFAULT_SERVER_PORT,
+                        help=f'tcp port to listen on, default {str(helpers.DEFAULT_SERVER_PORT)}')
     return parser.parse_args(cmd_args)
+
+
+def read_requests(source_clients, all_clients):
+    requests = {}
+    for client_socket in source_clients:
+        try:
+            data = client_socket.recv(helpers.TCP_MSG_BUFFER_SIZE)
+            requests[client_socket] = data
+        except:
+            print(f'Client disconnected: {client_socket.getpeername()}')
+            client_socket.close()
+            all_clients.remove(client_socket)
+    return requests
+
+
+def translate_requests(requests, target_clients, all_clients):
+    for author_socket in requests:
+        response = requests[author_socket]
+        for client_socket in target_clients:
+            try:
+                client_socket.send(response)
+            except:
+                print(f'Client disconnected: {client_socket.getpeername()}')
+                client_socket.close()
+                all_clients.remove(client_socket)
+
+
+def mainloop(cmd_args: argparse.Namespace):
+    server_socket = socket(AF_INET, SOCK_STREAM)
+    server_socket.bind((cmd_args.listen_address, cmd_args.listen_port))
+    server_socket.listen(helpers.CLIENTS_COUNT_LIMIT)
+    server_socket.settimeout(helpers.SERVER_SOCKET_TIMEOUT)
+    clients = []
+
+    while True:
+        try:
+            conn, addr = server_socket.accept()  # check for new connections
+        except OSError:
+            pass  # timeout, do nothing
+        else:
+            print(f'Client connected: {str(addr)}')
+            clients.append(conn)
+        finally:  # check for input data, if found - translate it to all clients
+            readable, writable, erroneous = [], [], []
+            try:
+                readable, writable, erroneous = select.select(clients, clients, clients, 0)
+            except:
+                pass  # if some client unexpectedly disconnected, do nothing
+
+            requests = read_requests(source_clients=readable, all_clients=clients)
+            translate_requests(requests, target_clients=writable, all_clients=clients)
 
 
 if __name__ == '__main__':
     args = parse_commandline_args(sys.argv[1:])
-    server_socket = sck.socket(sck.AF_INET, sck.SOCK_STREAM)
-    server_socket.bind((args.listen_address, args.listen_port))
-    server_socket.listen(CLIENTS_COUNT_LIMIT)
-    print('Waiting for incoming connections...')
-
-    while True:
-        client, addr = server_socket.accept()
-        print(f'connection from client {addr}')
-        message_bytes = client.recv(helpers.TCP_MSG_BUFFER_SIZE)
-        message = jim_msg_from_bytes(message_bytes)
-        print(f'message from client: {message}')
-        response = JimMessage()
-        response.set_field('response', 200)
-        response.set_time()
-        client.send(response.to_bytes())
-        client.close()
+    mainloop(args)
