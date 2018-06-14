6cc980cfea811f78ebf6a1734bd059bcda29ba15
dmitry-vs <dmitryselin@inbox.ru>
2018-06-14 12:07:02 +0300

    Added class DBStorageClient with tests

--- storage.py
+++ storage.py
@@ -1,13 +1,27 @@
 import sqlite3
 
 
-class DBStorageServer:
+class DBStorage:
     def __init__(self, database):
-        # connect to database, create it if not exists,
-        # create db schema if not exists (tables Clients, ClientContacts)
-        self.__conn = sqlite3.connect(database)
-        cursor = self.__conn.cursor()
-        cursor.executescript('''
+        self._conn = sqlite3.connect(database)
+        self._cursor = self._conn.cursor()
+
+    def __del__(self):
+        self._conn.close()
+
+    @property
+    def conn(self):
+        return self._conn
+
+    @property
+    def cursor(self):
+        return self._cursor
+
+
+class DBStorageServer(DBStorage):
+    def __init__(self, database):
+        super().__init__(database)
+        self._cursor.executescript('''
         CREATE TABLE IF NOT EXISTS `Clients`(
             `id`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, 
             `login` TEXT NOT NULL UNIQUE, 
@@ -16,8 +30,8 @@ class DBStorageServer:
             `last_connect_ip`	TEXT
         );
         ''')
-        cursor.execute('PRAGMA foreign_keys = ON;')
-        cursor.executescript('''
+        self._cursor.execute('PRAGMA foreign_keys = ON;')
+        self._cursor.executescript('''
         CREATE TABLE IF NOT EXISTS `ClientContacts` (
             `owner_id`	    INTEGER,
             `contact_id`	INTEGER,
@@ -26,31 +40,24 @@ class DBStorageServer:
             FOREIGN KEY(`contact_id`) REFERENCES `Clients`(`id`)
         );
         ''')
-        self.__conn.commit()
-
-    @property
-    def conn(self):
-        return self.__conn
+        self._conn.commit()
 
     def get_client_id(self, login: str):
         """ Returns client id by login from Clients table, or None if there is no such client """
-        cursor = self.__conn.cursor()
-        cursor.execute('SELECT `id` FROM `Clients` WHERE `login` == ?', (login,))
+        self._cursor.execute('SELECT `id` FROM `Clients` WHERE `login` == ?', (login,))
         try:
-            return cursor.fetchall()[0][0]
+            return self._cursor.fetchall()[0][0]
         except IndexError:
             return None
 
     def add_new_client(self, login: str):
         if not login:
             raise ValueError('login cannot be None nor empty')
-        cursor = self.__conn.cursor()
-        cursor.execute('INSERT INTO `Clients` VALUES (NULL, ?, NULL, NULL, NULL)', (login,))
-        self.__conn.commit()
+        self._cursor.execute('INSERT INTO `Clients` VALUES (NULL, ?, NULL, NULL, NULL)', (login,))
+        self._conn.commit()
 
     def update_client(self, client_id: int, connection_time: float, connection_ip: str, info=None):
-        cursor = self.__conn.cursor()
-        cursor.execute(
+        self._cursor.execute(
             """
             UPDATE `Clients` SET 
                 `info` = ?, 
@@ -59,25 +66,55 @@ class DBStorageServer:
             WHERE `id` == ?;
             """, (info, connection_time, connection_ip, client_id)
         )
-        self.__conn.commit()
+        self._conn.commit()
 
     def check_client_in_contacts(self, owner_id: int, client_id: int) -> bool:
-        cursor = self.__conn.cursor()
-        cursor.execute('SELECT COUNT() FROM `ClientContacts` WHERE `owner_id` == ? AND `contact_id` == ?;',
+        self._cursor.execute('SELECT COUNT() FROM `ClientContacts` WHERE `owner_id` == ? AND `contact_id` == ?;',
                        (owner_id, client_id))
-        return True if cursor.fetchall()[0][0] == 1 else False
+        return True if self._cursor.fetchall()[0][0] == 1 else False
 
     def add_client_to_contacts(self, owner_id: int, client_id: int):
-        cursor = self.__conn.cursor()
-        cursor.execute('INSERT INTO `ClientContacts` VALUES (?, ?);', (owner_id, client_id))
-        self.__conn.commit()
-
-    def __del__(self):
-        self.__conn.close()
+        self._cursor.execute('INSERT INTO `ClientContacts` VALUES (?, ?);', (owner_id, client_id))
+        self._conn.commit()
 
 
-class DBStorageClient:
-    pass
+class DBStorageClient(DBStorage):
+    def __init__(self, database):
+        # connect to database, create it if not exists,
+        # create db schema if not exists (tables Contacts, Messages)
+        super().__init__(database)
+        self._cursor.executescript('''
+        CREATE TABLE IF NOT EXISTS `Contacts`(
+            `id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+            `login`	TEXT NOT NULL UNIQUE
+        );
+        ''')
+        self._cursor.execute('PRAGMA foreign_keys = ON;')
+        self._cursor.executescript('''
+        CREATE TABLE IF NOT EXISTS `Messages` (
+            `id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+            `contact_id`	INTEGER NOT NULL,
+            `incoming`	INTEGER NOT NULL,
+            `text`	TEXT NOT NULL,
+            FOREIGN KEY(`contact_id`) REFERENCES `Contacts`(`id`)
+        );
+        ''')
+        self._conn.commit()
+
+    def add_new_contact(self, login: str):
+        self._cursor.execute('INSERT INTO `Contacts` VALUES(NULL, ?)', (login,))
+        self._conn.commit()
+
+    def get_contact_id(self, login: str):
+        self._cursor.execute('SELECT `id` FROM `Contacts` WHERE `login` == ?', (login,))
+        try:
+            return self._cursor.fetchall()[0][0]
+        except IndexError:
+            return None
+
+    def add_new_message(self, contact_id: int, incoming: bool, text: str):
+        self._cursor.execute('INSERT INTO `Messages` VALUES (NULL, ?, ?, ?)', (contact_id, int(incoming), text))
+        self._conn.commit()
 
 
 class FileStorage:
--- test_storage.py
+++ test_storage.py
@@ -1,13 +1,11 @@
 import pytest
 import sqlite3
 
-from storage import DBStorageServer
+from storage import DBStorageServer, DBStorageClient
 
 
 class TestDBStorageServer:
     test_db = ':memory:'
-    clients_table_name = 'Clients'
-    contacts_table_name = 'ClientContacts'
     test_login = 'TestLogin'
     test_time = 123456
     test_ip = '1.2.3.4'
@@ -17,16 +15,16 @@ class TestDBStorageServer:
     def setup(self):
         self.storage = DBStorageServer(self.test_db)
         self.conn = self.storage.conn
-        self.cursor = self.conn.cursor()
+        self.cursor = self.storage.cursor
 
     def test__init__tables_created(self):
         self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
         table_names = [t[0] for t in self.cursor.fetchall()]
-        assert self.clients_table_name in table_names
-        assert self.contacts_table_name in table_names
+        assert 'Clients' in table_names
+        assert 'ClientContacts' in table_names
 
     def test__get_client_id__client_exists__return_correct_id(self):
-        self.cursor.execute(f'INSERT INTO {self.clients_table_name} VALUES (NULL, ?, NULL, NULL, NULL)', (self.test_login,))
+        self.cursor.execute(f'INSERT INTO `Clients` VALUES (NULL, ?, NULL, NULL, NULL)', (self.test_login,))
         self.conn.commit()
         actual = self.storage.get_client_id(self.test_login)
         assert actual == 1
@@ -98,6 +96,15 @@ class TestDBStorageServer:
                             (first_client_id, second_client_id))
         assert self.cursor.fetchall()[0][0] == 1
 
+    def test__add_client_to_contacts_clients_add_each_other__no_errors(self):
+        self.storage.add_new_client(self.test_login)
+        self.storage.add_new_client(self.test_second_login)
+        first_client_id = self.storage.get_client_id(self.test_login)
+        second_client_id = self.storage.get_client_id(self.test_second_login)
+
+        self.storage.add_client_to_contacts(first_client_id, second_client_id)
+        self.storage.add_client_to_contacts(second_client_id, first_client_id)
+
     def test__add_client_to_contacts__client_already_in_contacts__raises(self):
         self.storage.add_new_client(self.test_login)
         self.storage.add_new_client(self.test_second_login)
@@ -132,3 +139,78 @@ class TestDBStorageServer:
     def test__check_client_in_contacts__input_incorrect__return_false(self):
         assert self.storage.check_client_in_contacts('qwerty', 'asdfgh') is False
         assert self.storage.check_client_in_contacts(None, None) is False
+
+
+class TestDBStorageClient:
+    test_db = ':memory:'
+    test_login = 'TestLogin'
+    test_second_login = 'TestLogin2'
+    test_message = 'Test message'
+
+    def setup(self):
+        self.storage = DBStorageClient(self.test_db)
+        self.conn = self.storage.conn
+        self.cursor = self.storage.cursor
+
+    def test__init__tables_created(self):
+        self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        table_names = [t[0] for t in self.cursor.fetchall()]
+        assert 'Contacts' in table_names
+        assert 'Messages' in table_names
+
+    def test__add_new_contact__not_added_yet__correct_row_added(self):
+        self.storage.add_new_contact(self.test_login)
+        self.cursor.execute('SELECT `login` FROM `Contacts` WHERE `id` == 1')
+        assert self.cursor.fetchall()[0][0] == self.test_login
+
+    def test__add_new_contact__already_added__raises(self):
+        self.storage.add_new_contact(self.test_login)
+        with pytest.raises(sqlite3.Error):
+            self.storage.add_new_contact(self.test_login)
+
+    def test__add_new_contact__input_incorrect__raises(self):
+        with pytest.raises(sqlite3.Error):
+            self.storage.add_new_contact(None)
+        with pytest.raises(sqlite3.Error):
+            self.storage.add_new_contact({})
+
+    def test__get_contact_id__contact_exists__return_correct_value(self):
+        self.storage.add_new_contact(self.test_login)
+        assert self.storage.get_contact_id(self.test_login) == 1
+
+    def test__get_contact_id__no_such_contact__return_none(self):
+        assert self.storage.get_contact_id(self.test_login) is None
+
+    def test__get_contact_id__input_none__return_none(self):
+        assert self.storage.get_contact_id(None) is None
+
+    def test__add_new_message__input_ok_incoming__correct_row_added(self):
+        self.storage.add_new_contact(self.test_login)
+
+        self.storage.add_new_message(1, True, self.test_message)
+
+        self.cursor.execute('SELECT * FROM `Messages`')
+        result = self.cursor.fetchall()
+        assert len(result) == 1
+        assert result[0] == (1, 1, int(True), self.test_message)
+
+    def test__add_new_message__input_ok_outcoming__correct_row_added(self):
+        self.storage.add_new_contact(self.test_login)
+
+        self.storage.add_new_message(1, False, self.test_message)
+
+        self.cursor.execute('SELECT * FROM `Messages`')
+        result = self.cursor.fetchall()
+        assert len(result) == 1
+        assert result[0] == (1, 1, int(False), self.test_message)
+
+    def test__add_new_message__input_incorrect__raises(self):
+        with pytest.raises(sqlite3.Error):
+            self.storage.add_new_message(None, True, self.test_message)
+        self.storage.add_new_contact(self.test_login)
+        with pytest.raises(TypeError):
+            self.storage.add_new_message(1, None, self.test_message)
+        with pytest.raises(sqlite3.Error):
+            self.storage.add_new_message(1, True, None)
+        with pytest.raises(sqlite3.Error):
+            self.storage.add_new_message(1, False, None)
